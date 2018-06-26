3b59c91ca6eeb365eed216403b5c78eee7778493
wanggua520 <40431958+wanggua520@users.noreply.github.com>
2018-06-26 17:44:37 +0800

    Add files via upload

--- 115.cpp
+++ 115.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include<windows.h>
 int a,b,c,d,e,f;
-struct zggz
+struct worker
 {
 char num[10];
 char name[30];
@@ -16,18 +16,16 @@ float yfgz;
 float grsds;
 float salary;
 };
-int dq(struct worker wo[]);
-void show(void);
-void bc(struct worker wo[]); 
+int dq(struct worker zggz[]);
+void write(struct worker zggz[]); 
 void ghcz();
 void xmcz();
-void xmsc();
-void ghsc();
- struct zggz wo[100];//保存所有的职工信息
+int grsds();
+ struct worker zggz[100];//保存所有的职工信息
 int Number=0; //记录总的职工人数
 int i,j,n,m,sum[100],num[100],p=0; 
-int score[100][20],lower,maxer,temp=0; 
-void read(struct zggz S[])
+int temp=0; 
+void read(struct worker S[])
 {
 
 	
@@ -43,7 +41,7 @@ void read(struct zggz S[])
 
        }
        //如果文件打开成功，那么进行读数据
-       struct zggz s;
+       struct worker s;
        while (!feof(fp))
 
        {
@@ -72,40 +70,94 @@ void read(struct zggz S[])
 
        }
        n=Number;
-       printf("\n有%d个员工信息被读入\n",Number);
+       printf("\n有%d个员工信息被读入\n",n);
 
        fclose(fp);
 
        
 }
-void show()
-{ 
-printf("\n");
-printf("\t工资管理系统 \n\n");
-printf("\t请选择功能 （输入0~6）\n");
-printf("\t\n\n");
-printf("\t1. 查询\n");
-printf("\t2. 修改\n");
-printf("\t3. 添加\n");
-printf("\t4. 删除\n");
-printf("\t5. 保存\n");
-printf("\t6. 预览\n");
-printf("\t0. 退出\n");
-printf("\t\n");
-}
-void bc(struct zggz *wo)
+void fwrite(struct worker *zggz)
 {
 FILE *fp=NULL;
 fp=fopen("gx.dat","ab+");
-fwrite(wo,sizeof(struct zggz),1,fp);
+if(zggz>0)
+{
+	fwrite(zggz,sizeof(struct worker),1,fp);}
 fclose(fp);
+}
+//保存数据
+void write() 
+{
+   int i;
+   FILE*fp;
+   if ((fp=fopen("gx.dat","wb"))==NULL)  //创建文件并判断是否能打开
+   {
+     printf ("cannot open file\n");
+     exit(0);
+   }
+   for (i=0;i<m;i++) //将内存中职工的信息输出到磁盘文件中去
+   if (fwrite(&zggz,sizeof(struct worker),1,fp)!=1)
+   printf("file write error\n");
+   printf("保存成功！！");
+
+   fclose(fp);
+}
+//查找职工信息
+void find() 
+{
+int n=0;
+
+printf("1 按工号查找\n");
+ printf("2 按姓名查找\n");
+scanf("%d",&n);
+switch(n)
+{
+case 1: ghcz();
+break;
+case 2: xmcz();
+break;
+default: printf("输入错误!\n");
+break;
+} 
+}
+void xmcz()//按姓名查找 
+{ 
+char name[30];
+int i=0;
+printf("请输入要查找职工姓名:");
+scanf("%s",name);
+system("cls");
+printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
+for (i=0;i<Number;i++)
+{
+if (strcmp(name,zggz[i].name)==0)
+{
+printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%fd\t%d\t%d\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
+}
+}
+}
+void ghcz()//按工号查找 
+{
+char gonghao[10];
+int i=0;
+printf("请输入要查找职工工号:");
+scanf("%s",gonghao);
+system("cls");
+printf("\t姓名\t工号\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
+for (i=0;i<Number;i++)
+{
+if (strcmp(gonghao,zggz[i].num)==0)//比较工号
+{
+printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
+}
+}
 }
 //添加职工信息
 void add() 
 {
 int numberTemp=0;
 int i=0;
-struct zggz temp; 
+struct worker temp; 
 printf("添加职工个数：");
 scanf("%d",&numberTemp);
 for(i=0;i<numberTemp;i++)
@@ -124,21 +176,17 @@ scanf("%d",&temp.zwjt);
 printf("校绩工资：");
 scanf("%d",&temp.xjgz);
 printf("应发工资：");
-scanf("%d",&temp.yfgz);
+temp.yfgz=temp.gwgz+temp.xjsz+temp.zwjt+temp.xjgz;
 printf("个人所得税：");
-scanf("%d",&temp.grsds);
+//temp.grsds=
 printf("实发工资：");
-scanf("%d",&temp.salary);
-wo[Number++]=temp; //写入到数组
-bc(&temp); //写入到文件
+
+zggz[Number++]=temp; //写入到数组
+fwrite(&temp); //写入到文件
 }
 printf("添加成功\n");
 }
-//修改成员数据
-void modify()
-{
 
-}
 //预览
 void list() 
 {
@@ -147,186 +195,130 @@ printf("以下是全部职工\n");
 printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
 while(i<Number)
 {
-printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",wo[i].num,wo[i].name,wo[i].gwgz,wo[i].xjsz,wo[i].zwjt,wo[i].xjgz,wo[i].yfgz,wo[i].grsds,wo[i].salary);
+printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
 i++;
 }
 }
-//查找职工信息
-void find() 
+//修改成员数据
+void modify()
 {
-int n=0;
-
-printf("1 按工号查找\n");
- printf("2 按姓名查找\n");
-scanf("%d",&n);
-switch(n)
-{
-
-case 1: ghcz();
-break;
-case 2: xmcz();
-break;
-default: printf("该员工不存在!\n");
-break;
-} 
-}
-void xmcz()//按姓名查找 
-{ 
-char name[30];
-int i=0;
-printf("请输入要查找职工姓名:");
-scanf("%s",name);
-system("cls");
-printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
-for (i=0;i<Number;i++)
-{
-if (strcmp(name,wo[i].name)==0)
-{
-printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%fd\t%d\t%d\n",wo[i].num,wo[i].name,wo[i].gwgz,wo[i].xjsz,wo[i].zwjt,wo[i].xjgz,wo[i].yfgz,wo[i].grsds,wo[i].salary);
-}
-}
-}
-void ghcz()//按工号查找 
-{
-char num[10];
-int i=0;
-printf("请输入要查找职工工号:");
+  char gonghao[10];
+  int n=0; 
+  printf("请输入要修改职工工号:");
 scanf("%s",num);
-system("cls");
-printf("\t姓名\t工号\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
-for (i=0;i<Number;i++)
+printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
+for (n=0;n<Number;n++)
 {
-if (strcmp(num,wo[i].name)==0)
-{
-printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",wo[i].num,wo[i].name,wo[i].gwgz,wo[i].xjsz,wo[i].zwjt,wo[i].xjgz,wo[i].yfgz,wo[i].grsds,wo[i].salary);
+	if(strcmp(gonghao,zggz[n].num)==0)
+	{
+		printf("工号：");
+        scanf("%s",zggz[n].num);
+        printf("姓名：");
+        scanf("%s",zggz[n].name);
+        printf("岗位工资：");
+        scanf("%d",&zggz[n].gwgz);
+        printf("薪级工资：");
+        scanf("%d",&zggz[n].xjsz);
+        printf("职务工资：");
+        scanf("%d",&zggz[n].zwjt);
+        printf("校绩工资：");
+		scanf("%d",&zggz[n].xjgz);
+        
+        //wo[Number++]=temp; //写入到数组
+        //fwrite(&wo); //写入到文件
+		
 }
+
+}       
+	
 }
-}
+
 //删除职工信息
 void del() 
 {
-int n=0;
-printf("1 按工号删除\n");
-printf("2 按姓名删除\n");
-scanf("%d",&n);
-switch(n)
-{
-case 1: ghsc();
-break;
-case 2: xmsc();
-break;
-default: printf("该员工不存在!\n");
-break;
-}
-}
-void ghsc()//按工号删除 
-{
 FILE *fp=NULL;
-char id[60];
+char gonghao[60];
 int i=0;
 int j=0;
 printf("请输入要删除的职工工号：");
-scanf("%s",id);
+scanf("%s",gonghao);
 for(i=0;i<Number;i++)
 {
-if (strcmp(id,wo[i].num)==0) 
+if (strcmp(gonghao,zggz[i].num)==0) 
 {
+	 printf("\n已找到此人，原始记录为：\n");
+            printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
+   printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
+ 
+            printf("\n确实要删除此人信息请按1\n");
+   scanf("%d",&n);
+
+            if(n==1)   /*如果删除，则其他的信息都往上移一行*/
+   {
 for (j=i;j<Number-1;j++)
 {
-wo[j]=wo[j+1];
+zggz[j]=zggz[j+1];
 }
 Number--;
 }
+
 }
 //将剩余数据写入文件 擦除以前的数据
 fp=fopen("gx.dat","wb");
 for (i=0;i<Number;i++)
 {
-fwrite(&wo[i],sizeof(struct zggz),1,fp);
+fwrite(&zggz[i],sizeof(struct worker),1,fp);
 }
 fclose(fp);
+
 printf("删除成功;\n");
 }
-void xmsc()//按姓名删除 
-{
-FILE *fp=NULL;
-char name[30];
-int i=0;
-int j=0;
-printf("请输入要删除的职工姓名：");
-scanf("%s",name);
-for(i=0;i<Number;i++)
-{
-if (strcmp(name,wo[i].name)==0)
-{
-for (j=i;j<Number-1;j++)
-{
-wo[j]=wo[j+1];
 }
-Number--;
-}
-}
-//将剩余数据写入文件 擦除以前的数据
-fp=fopen("gx.dat","wb");
-for (i=0;i<Number;i++)
+int grsds()
 {
-fwrite(&wo[i],sizeof(struct zggz),1,fp);
-}
-fclose(fp);
-printf("删除成功;\n");            
 
-}
-
-//保存数据
-void write() 
-{
-   int i;
-   FILE*fp;
-   if ((fp=fopen("gx.dat","wb"))==NULL)  //创建文件并判断是否能打开
-   {
-     printf ("cannot open file\n");
-     exit(0);
-   }
-   for (i=0;i<m;i++) //将内存中职工的信息输出到磁盘文件中去
-   if (fwrite(&wo,sizeof(struct zggz),1,fp)!=1)
-   printf("file write error\n");
-   printf("保存成功！！");
-
-   fclose(fp);
-}
-void back()
-{
-printf("\t按Enter键返回\n");
+return 0;
 }
 void main()
 {
-	struct zggz wo[100];
+	struct worker zggz[100];
 	
 	int choose=0;
 while(1) 
 { 
-show();
+printf("\n");
+printf("\t工资管理系统 \n\n");
+printf("\t请选择功能 （输入0~6）\n");
+printf("\t\n\n");
+printf("\t1. 查询\n");
+printf("\t2. 修改\n");
+printf("\t3. 添加\n");
+printf("\t4. 删除\n");
+printf("\t5. 保存\n");
+printf("\t6. 预览\n");
+printf("\t0. 退出\n");
+printf("\t\n");
 printf("\t\t====>请选择：");
 scanf("%d",&choose);
 system("cls");
 switch(choose)
 {
 case 1: find();
-back();// 查找职工信息 
+// 查找职工信息 
 break;
  case 2: modify();
-back();//修改职工信息
+//修改职工信息
 case 3: add();
-back();//添加职工信息
+//添加职工信息
 break; 
 case 4: del(); 
-back();//删除职工信息
+//删除职工信息
 break;
 case 5:write();
-back();//保存数据 
+//保存数据 
 break;
 case 6: list();
-back();//预览
+//预览
 break; 
 case 0: exit(0);//退出
 break;
@@ -340,6 +332,6 @@ system("cls");
 }
 }
 
-read(wo);
+read(zggz);
 
 }
