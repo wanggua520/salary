98a0f7eb138d1367cb80d5da827b3b31e9a88502
wanggua520 <40431958+wanggua520@users.noreply.github.com>
2018-06-29 11:52:39 +0800

    Add files via upload

--- 115.cpp
+++ 115.cpp
@@ -15,43 +15,56 @@ struct worker
     float yfgz;
     float grsds;
     float salary;
-};
-    struct worker zggz[100];//保存所有的职工信息
-    int Number=0; //记录总的职工人数
-    int i,j,n,m,sum[100],num[100],p=0; 
+}zggz[100],*p;
+    //struct worker zggz[100];//保存所有的职工信息
+    int n=0; //记录总的职工人数
+    int i,j,m,sum[100],num[100]; 
     int temp=0; 
-    int read(struct worker S[])
+/*int read()
 {
 
+    FILE *fp;
 	int i=0;
-	FILE *fp;
-	    if((fp=fopen("gx.dat","r"))==NULL)//当前目录下是否存在gx.dat文件
+	p=zggz;
+	if((fp=fopen("gx.dat","rt"))==NULL)//当前目录下是否存在gx.dat文件
 	{
-	    printf("没有文件");
+	    printf("打开文件失败");
 	    getch();
 	    exit(-1);
 	}
-	    struct worker s;
-            while(!feof(fp))
+		
+		for(i=0;i<=100;i++)
+		{
+			fread(p,sizeof(struct worker),1,fp);
+            n++;
+			p++;
+			if(feof(fp))
+			{
+				printf("打开文件失败!\n");
+				break;
+			}
+		}
+	    
+           /* while(!feof(fp))
 			{
 	            fscanf(fp,"%s %s %f %f %f %f %f %f %f\n",&(s.num),&(s.name),&(s.gwgz),&(s.xjsz),&(s.zwjt),
                 &(s.xjgz),&(s.yfgz),&(s.grsds),&(s.salary));
             	printf("%s %s %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f\n",s.num,s.name,s.gwgz,s.xjsz,s.zwjt,s.xjgz,s.yfgz,s.grsds,s.salary); 
-	            n++;
 	            i++;
 			}
 	
 	       printf("有%d工人信息录入",n);//输出职工人数
 	       fclose(fp);
            return 0;
-}
-/*void read(struct worker S[])
+}*/
+void read()
 {
 
-	
+	int i=0;
        FILE *fp;//定义文件指针
+	   p=zggz;
 
-       if ((fp = fopen("gx.dat", "r")) == NULL)
+       if ((fp = fopen("gx.dat", "rt+")) == NULL)
 
        {
 
@@ -60,51 +73,23 @@ struct worker
               exit(-1);
 
        }
-       //如果文件打开成功，那么进行读数据
-       struct worker s;
        while (!feof(fp))
 
        {
-
-              fscanf(fp, "%s\t", s.num);
-
-              fscanf(fp, "%s\t", s.name);
-
-              fscanf(fp, "%.2f\t", &s.gwgz);
-
-			  fscanf(fp, "%.2f\t", &s.xjsz);
-			  
-			  fscanf(fp, "%.2f\t", &s.zwjt);
-			  
-			  fscanf(fp, "%.2f\t", &s.xjgz);
-
-              fscanf(fp, "%.2f\t", &s.yfgz);
-
-              fscanf(fp, "%.2f\n", &s.grsds);
-
-              fscanf(fp, "%.2ff\n", &s.salary);
-
-              S[Number]=s;
-
-              Number++;
-
-       }
-       n=Number;
+		      printf("文件打开成功\n");
+			  printf("\t姓名\t工号\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
+			  fscanf(fp,"%s %s %f %f %f %f %f %f %f\n",&(p[i].num),&(p[i].name),&(p[i].gwgz),&(p[i].xjsz),&(p[i].zwjt),
+              &(p[i].xjgz),&(p[i].yfgz),&(p[i].grsds),&(p[i].salary));
+              printf("%s %s %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f\n",p[i].num,p[i].name,p[i].gwgz,p[i].xjsz,p[i].zwjt,p[i].xjgz,p[i].yfgz,p[i].grsds,p[i].salary); 
+              i++;
+			  n=i;
+	   }
+       
        printf("\n有%d个员工信息被读入\n",n);
 
        fclose(fp);
 
        
-}*/
-void fwrite(struct worker *zggz)
-{
-    FILE *fp=NULL;
-    fp=fopen("gx.dat","ab+");
-    if(zggz>0)
-	{
-	    fwrite(zggz,sizeof(struct worker),1,fp);
-	}
-        fclose(fp);
 }
 //保存数据
 void write() 
@@ -125,53 +110,65 @@ void write()
 //查找职工信息
 void find() 
 {
+	     
     char gonghao[10];
     int i=0;
     printf("请输入要查找职工工号:");
     scanf("%s",gonghao);
     system("cls");
     printf("\t姓名\t工号\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
-    for (i=0;i<Number;i++)
+    for (i=0;i<n;i++)
 	{
         if (strcmp(gonghao,zggz[i].num)==0)//比较工号
 		{
-            printf("\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-			zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
+            printf("%s %s %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
 		}
 	}
 }
 //添加职工信息
 void add() 
-{
-     int numberTemp=0;
+{ 
+	 
+     //int numberTemp=0;
+	 n++;
      int i=0;
-     struct worker temp; 
-     printf("添加职工个数：");
-     scanf("%d",&numberTemp);
-     for(i=0;i<numberTemp;i++)
+     //struct worker temp; 
+     printf("添加职工：\n");
 	 {
-         printf("输入第%d个职工信息\n",i+1);
+		 
+        // printf("输入第%d个职工信息\n",i+1);
          printf("工号：");
-         scanf("%s",temp.num);
+         scanf("%s",zggz[n-1].num);
          printf("姓名：");
-         scanf("%s",temp.name);
+         scanf("%s",zggz[n-1].name);
          printf("岗位工资：");
-         scanf("%d",&temp.gwgz);
+         scanf("%d",&zggz[n-1].gwgz);
          printf("薪级工资：");
-         scanf("%d",&temp.xjsz);
+         scanf("%d",&zggz[n-1].xjsz);
          printf("职务工资：");
-         scanf("%d",&temp.zwjt);
+         scanf("%d",&zggz[n-1].zwjt);
          printf("校绩工资：");
-         scanf("%d",&temp.xjgz);
-         /*printf("应发工资：");
-         
+         scanf("%d",&zggz[n-1].xjgz);
+         printf("应发工资：");
+         zggz[n-1].yfgz=zggz[n-1].gwgz+zggz[n-1].xjsz+zggz[n-1].zwjt+zggz[n-1].xjgz;
          printf("个人所得税：");
-         grsds();
-         printf("实发工资：");*/
+         grsds(zggz);
+         printf("实发工资：");
+		 zggz[n-1].salary=zggz[n-1].yfgz-zggz[n-1].grsds;
+	 }
+         FILE *fp=NULL;
+         fp=fopen("gx.dat","ab+");
+         if(zggz>0)
+		 {
+	          fwrite(zggz,sizeof(struct worker),1,fp);
+		 }
+         fclose(fp);
 
-         zggz[Number++]=temp; //写入到数组
-         fwrite(&temp); //写入到文件
-     }
+        // zggz[n++]=temp; //写入到数组
+        // write(); //写入到文件
+		 
+     
+	     
          printf("添加成功\n");
 }
 
@@ -181,43 +178,52 @@ void list()
     int i=0;
     printf("以下是全部职工\n");
     printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
-    while(i<Number)
+    while(i<n)
 	{
 		 zggz[i].yfgz=(zggz[i].gwgz+zggz[i].xjsz+zggz[i].zwjt+zggz[i].xjgz);
-		 //grsds();
+		 //grsds(zggz);
 		 zggz[i].salary=zggz[i].yfgz-zggz[i].grsds;
-         printf("\t%s\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
+         printf("%s %s %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f %5.1f\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
          i++;
 	}
 }
 //修改成员数据
 void modify()
 {
+	FILE *fp=NULL;
     char gonghao[10];
-	int n=0; 
+	int m,i; 
     printf("请输入要修改职工工号:");
-    scanf("%s",num);
-    printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
-    for (n=0;n<Number;n++)
+    scanf("%s",gonghao);
+    for (m=0;m<n;m++)
 	{
-	    if(strcmp(gonghao,zggz[n].num)==0)
+	    if(strcmp(gonghao,zggz[m].num)==0)
 		{
-		    printf("工号：");
-            scanf("%s",num);
-            printf("姓名：");
-            scanf("%s",zggz[n].name);
-            printf("岗位工资：");
-            scanf("%d",&zggz[n].gwgz);
-            printf("薪级工资：");
-            scanf("%d",&zggz[n].xjsz);
-            printf("职务工资：");
-            scanf("%d",&zggz[n].zwjt);
-            printf("校绩工资：");
-		    scanf("%d",&zggz[n].xjgz);
+			printf("\n已找到此人，原始记录为：\n");
+            printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
+            printf("\t%s\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",zggz[m].num,zggz[m].name,zggz[m].gwgz,zggz[m].xjsz,zggz[m].zwjt,zggz[m].xjgz,zggz[m].yfgz,zggz[m].grsds,zggz[m].salary);
+            printf("\n确实要修改此人信息请按1\n");
+            scanf("%d",&f);
+			if(f==1)
+			{ 
+			    i=m;
+		        printf("工号：");
+                scanf("%s",zggz[i].num);
+                printf("姓名：");
+                scanf("%s",zggz[i].name);
+                printf("岗位工资：");
+                scanf("%d",&zggz[i].gwgz);
+                printf("薪级工资：");
+                scanf("%d",&zggz[i].xjsz);
+                printf("职务工资：");
+                scanf("%d",&zggz[i].zwjt);
+                printf("校绩工资：");
+	    	    scanf("%d",&zggz[i].xjgz);
         
-            //wo[Number++]=temp; //写入到数组
-            //fwrite(&wo); //写入到文件
-		
+           // zggz[n++]=temp; //写入到数组
+            //fwrite(&temp);//写入到文件
+                write();
+			}
 		}
 
 	}       
@@ -227,53 +233,50 @@ void modify()
 //删除职工信息
 void del() 
 {
-    FILE *fp=NULL;
-    char gonghao[60];
-    int i=0;
-    int j=0;
-    printf("请输入要删除的职工工号：");
-    scanf("%s",gonghao);
-    for(i=0;i<Number;i++)
-	{
-        if (strcmp(gonghao,zggz[i].num)==0) 
-		{
-	        printf("\n已找到此人，原始记录为：\n");
-
+   FILE *fp=NULL;
+char gonghao[60];
+int i=0;
+int j=0;
+int f;
+printf("请输入要删除的职工工号：");
+scanf("%s",gonghao);
+for(i=0;i<n;i++)
+{
+if (strcmp(gonghao,zggz[i].num)==0) 
+{
+	 printf("\n已找到此人，原始记录为：\n");
             printf("\t工号\t姓名\t岗位工资\t薪级工资\t职务工资\t校绩工资\t应发工资\t个人所得税\t实发工资\n");
-
-            printf("\t%s\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
+   printf("\t%s\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",zggz[i].num,zggz[i].name,zggz[i].gwgz,zggz[i].xjsz,zggz[i].zwjt,zggz[i].xjgz,zggz[i].yfgz,zggz[i].grsds,zggz[i].salary);
  
             printf("\n确实要删除此人信息请按1\n");
-            scanf("%d",&n);
+   scanf("%d",&f);
 
-            if(n==1)   /*如果删除，则其他的信息都往上移一行*/
-			{
-                for (j=i;j<Number-1;j++)
-				{
-                     zggz[j]=zggz[j+1];
-				}
-                Number--;
-			}
-
-		}
-        //将剩余数据写入文件 擦除以前的数据
-        fp=fopen("gx.dat","wb");
-        for (i=0;i<Number;i++)
-	
-		
-		{
-             fwrite(&zggz[i],sizeof(struct worker),1,fp);
-		}
-             fclose(fp);
- 
-             printf("删除成功;\n");
-	}
-}
-int grsds()
+            if(f==1)   /*如果删除，则其他的信息都往上移一行*/
+   
+for (j=i;j<n-1;j++)
 {
-    while(i<Number)
+zggz[j]=zggz[j+1];
+}
+n--;
+
+
+}
+//将剩余数据写入文件 擦除以前的数据
+fp=fopen("gx.dat","wb");
+for (i=0;i<n;i++)
+{
+fwrite(&zggz[i],sizeof(struct worker),1,fp);
+}
+fclose(fp);
+
+printf("删除成功;\n");
+}
+}
+int grsds(struct worker *zggz)
+{
+    while(i<n)
 	{
-	//for(int i=0;i<20;i++)
+	for(int i=0;i<20;i++)
 		if(zggz[i].yfgz<=500)
 		zggz[i].grsds=zggz[i].yfgz*0.05;
 
@@ -304,11 +307,9 @@ i++;	}
                                               return zggz[i].grsds;
 }
 void main()
-{
-	struct worker zggz[100];
-	
+{	
 	int choose=0;
-
+    read();
      while(1) 
 	 { 
          printf("\n");
@@ -358,5 +359,5 @@ void main()
 			 }
 
 	 }
-read(zggz);
+
 }
